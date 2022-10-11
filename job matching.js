c6a6f3c954fc80bc468fe29c94b41e7b289d760a
prajvalsinghOD <singhprajval91@gmail.com>
2022-10-11 17:41:58 +0530

    job matching

---
+++ job matching.js
@@ -0,0 +1,17 @@
+function match(candidate, job) {
+
+    if(candidate.minSalary === undefined || job.maxSalary===undefined)
+      {
+        throw "Error";
+      }
+    else {
+      let minSal = candidate.minSalary - candidate.minSalary * 0.10; 
+      if(minSal<=job.maxSalary)
+        {
+          return true;
+        }
+      else{
+        return false;
+      }
+    }
+  }
